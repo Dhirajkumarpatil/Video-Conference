ff3526200d108cc7d67924729518387ffe231029
Dhirajkumar <=>
2026-05-16 11:09:40 +0530

    Api Modified

--- Frontend/src/contents/authentication.jsx
+++ Frontend/src/contents/authentication.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 export const AuthContext = createContext({});
 
 const client = axios.create({
-  baseURL: "http://localhost:8000/api/v1/user"
+  baseURL: "https://my-backend-ffjr.onrender.com"
 });
 
 export const AuthProvider = ({ children }) => {
